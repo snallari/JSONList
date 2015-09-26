b86c9f6d65428b14884cab9c2a7e11e67eb9376b
Shruti Nallari <shruti.nallari@gmail.com>
2015-09-25 21:21:49 -0400

    AF

---
+++ MyApplication/app/src/main/java/com/example/shrutinallari/myapplication/VolleySingleton.java
@@ -0,0 +1,50 @@
+package com.example.shrutinallari.myapplication;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by shrutinallari on 9/25/15.
+ */
+public class VolleySingleton {
+    private static VolleySingleton mInstance = null;
+    private ImageLoader mImageLoader;
+    private RequestQueue mRequestQueue;
+
+    private VolleySingleton() {
+        mRequestQueue = Volley.newRequestQueue(MyApplication.getmAppContext());
+        mImageLoader = new ImageLoader(this.mRequestQueue, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
+
+            @Override
+            public Bitmap getBitmap(String url) {
+                return mCache.get(url);
+            }
+
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                mCache.put(url, bitmap);
+            }
+        });
+
+    }
+
+    public static VolleySingleton getInstance() {
+        if (mInstance == null) {
+            mInstance = new VolleySingleton();
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        return this.mRequestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        return this.mImageLoader;
+    }
+}
